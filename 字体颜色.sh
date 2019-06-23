1fece317e7a43958ffcdf93a3e244545e962fd55
QiuGuangWEi <52090804+QiuGuangWEi@users.noreply.github.com>
2019-06-23 04:18:04 -0500

    Create 字体颜色.sh

---
+++ 字体颜色.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+cecho(){
+echo -e "\033[$1m$2\033[0m"
+}
+
+cecho 31 ABCDEFG
+cecho 32 ABCDEFG
+cecho 33 ABCDEFG
+cecho 34 ABCDEFG
+cecho 35 ABCDEFG
+cecho 36 ABCDEFG
+cecho 37 ABCDEFG
+cecho 38 ABCDEFG
+cecho 39 ABCDEFG
+cecho 40 ABCDEFG
+cecho 41 你好吗
